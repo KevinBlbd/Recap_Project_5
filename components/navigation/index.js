f28dfdbcb7b693f1901e95fd5a7905e2a5727eda
Gabor Szederkenyi <gaborszederkenyi@MacBook-Air-von-Gabor.local>
2023-04-05 15:10:06 +0200

    changes 2nd day

--- components/navigation/index.js
+++ components/navigation/index.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
+
 export default function NavigationBar() {
   return (
     <ul>
       <li>
-        <Link href="../pages/spotlight/index.js">see spotlights</Link>
+        <Link href="/">see spotlights</Link>
       </li>
       <li>
-        <Link href="../pages/artPieces/index.js">see all works</Link>
+        <Link href="/artPieces">see all works</Link>
       </li>
     </ul>
   );
